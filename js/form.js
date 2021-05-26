b1b49087d251d8265a584a8c250efa0437b57088
Saachimakhija123 <76513702+Saachimakhija123@users.noreply.github.com>
2021-05-26 13:02:28 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,54 @@
+class Form{
+    constructor(){
+       this.input = createInput("Name");
+       this.button = createButton('Play');
+       this.greeting = createElement('h2');
+      
+       this.reset = createButton('Reset');
+    }
+    hide() {
+        this.greeting.hide();
+        this.button.hide();
+        this.input.hide();
+        this.title.hide();
+    }
+    display() {
+      
+        this.title = createElement('h1');
+        this.title.html("FRUIT CATCHER");
+         this.title.position(350, 150);
+         this.title.style('color',  'blue');
+      
+        
+       
+        
+        
+        this.input.position(550,400);
+        
+        this.button.position(560,500);
+     
+        this.reset.position(900, 660);
+     
+        this.button.mousePressed(() => {
+            this.input.hide();
+            this.button.hide();
+            player.name = this.input.value();
+            playerCount += 1;
+            player.index = playerCount;
+            player.update();
+            player.updateCount(playerCount);
+            this.greeting.html("Hello " + player.name)
+            this.greeting.position(400,250);
+            this.greeting.style('color', 'blue');
+            this.greeting.style('font-size', '100px');
+        });
+
+        this.reset.mousePressed(() => {
+            player.updateCount(0);
+            game.update(0);
+            var playerInfoRef=database.ref('players');
+            playerInfoRef.remove();
+        });
+
+    }
+}
